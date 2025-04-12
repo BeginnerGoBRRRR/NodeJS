2679bf538360da9d4601debb218644eea1cf2f78
Phuong <phuongit1306@gmail.com>
2025-04-12 21:49:46 +0700

    notification & orderController

---
+++ controllers/orderController.js
@@ -0,0 +1,38 @@
+const Order = require('../schemas/order');
+
+exports.getAllOrders = async (req, res) => {
+  try {
+    const orders = await Order.find().populate('user products.product');
+    res.json(orders);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.createOrder = async (req, res) => {
+  try {
+    const order = new Order(req.body);
+    await order.save();
+    res.status(201).json(order);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+exports.updateOrder = async (req, res) => {
+  try {
+    const order = await Order.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(order);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+exports.deleteOrder = async (req, res) => {
+  try {
+    await Order.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Order deleted' });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
